63e8d965339ab0e74f00a41b621fe86a23436a8e
Ryan Samarajeewa <ryan.samarajeewa@gmail.com>
2018-10-07 11:33:02 -0700

    Add documentation

--- lighthouse9000.js
+++ lighthouse9000.js
@@ -10,32 +10,40 @@ const GRID = [
   ["", "^", "", "~", "~", "", "", "", "", ""],
   ["", "^", "", "", "~", "~", "", "", "", ""],
 ];
-    
+
+// Returns the number of rows in GRID
 function countRows(){
   return GRID.length;
 }
 
+// Returns the number of columns in GRID
 function countColumns(){
   return GRID[0].length;
 }
 
+// Returns the dimensions of GRID
 function gridSize(){
   return countColumns() + ' x ' + countRows();
 }
 
+// Returns the number of cells in GRID
 function totalCells(){
   return countRows() * countColumns();
 }
 
+// Given the coordinate as an argument, 
+// returns the number of the column
 function convertColumn(coordinate){
   const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   return letters.indexOf(coordinate.charAt(0));
 }
 
+// Returns the contents of a cell given its coordinate
 function lightCell(coordinate){
   return GRID[coordinate.charAt(1)-1][convertColumn(coordinate)];
 }
 
+// Returns whether a cell is a rock ('^')
 function isRock(coordinate){
   return lightCell(coordinate) == '^';
 }
